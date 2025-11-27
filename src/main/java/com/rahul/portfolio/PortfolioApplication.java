3efa5413586fe6141730a38075e57d59be23ced3
chinthapally rahul <rahulchinthapally4@gmail.com>
2025-11-27 18:46:22 +0530

    Create PortfolioApplication with basic Spring Boot setup

---
+++ src/main/java/com/rahul/portfolio/PortfolioApplication.java
@@ -0,0 +1,20 @@
+package com.rahul.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PortfolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "<h2>Rahul Portfolio Backend Running Successfully ✔️</h2>";
+    }
+}
